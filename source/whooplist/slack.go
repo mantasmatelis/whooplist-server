0b5e246b0925e03f58024b2236714f13b8e0408a
Mantas Matelis <me@mantasmatelis.com>
2014-03-14 00:16:51 -0400

    Removed unnecessary logging

--- source/whooplist/slack.go
+++ source/whooplist/slack.go
@@ -2,7 +2,6 @@ package whooplist
 
 import (
 	"bytes"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -10,7 +9,6 @@ import (
 var slackUrl = "https://whooplist.slack.com/services/hooks/incoming-webhook?token=GsURXK3zks9I7T3JhKBTOW0m"
 
 func SlackPostError(msg string) {
-	log.Print("{\"text\": \"" + msg + "\"}")
 	msg = strings.Replace(msg, "\n", "\\n", -1)
 	_, _ = http.Post(slackUrl, "text/json", bytes.NewBufferString("{\"text\": \""+msg+"\"}"))
 }
